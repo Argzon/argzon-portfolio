b9a07864982e1b9cbacb3247db8a2fe8c9c17897
Argzon <argzon01@gmail.com>
2025-03-27 02:45:56 +0100

    Added the og image for homepage

--- src/app/home/layout.js
+++ src/app/home/layout.js
@@ -1,7 +1,30 @@
+import HomepageOG from "../../../public/assets/opengraph-images/homepage-og-image.jpg";
+
 export const metadata = {
   title: "Interface Designer - Argzon Haziraj",
   description: "Building websites & apps for immersive digital experiences.",
-  url: "/",
+  url: "https://argzonhaziraj.com",
+  image: { HomepageOG },
+  openGraph: {
+    type: "website",
+    locale: "en_US",
+    url: "https://argzonhaziraj.com",
+    title: "Interface Designer - Argzon Haziraj",
+    description: "Building websites & apps for immersive digital experiences.",
+    images: [
+      {
+        url: { HomepageOG },
+        width: 1200,
+        height: 675,
+        alt: "Argzon Haziraj - Interface Designer",
+      },
+    ],
+  },
+  twitter: {
+    handle: "@Argzon6",
+    site: "@Argzon6",
+    cardType: "summary_large_image",
+  },
 };
 
 export default function HomeLayout({ children }) {
