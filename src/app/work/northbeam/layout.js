f7d79e54b6aa9a22262c07b690e8e517568fe13d
Argzon <argzon01@gmail.com>
2025-03-27 04:10:09 +0100

    Added metadata for all use cases

--- src/app/work/northbeam/layout.js
+++ src/app/work/northbeam/layout.js
@@ -1,8 +1,43 @@
 export const metadata = {
+  metadataBase: new URL("https://argzonhaziraj.com"),
   title: "Northbeam - Argzon's Portfolio",
   description:
     "A complete UX/UI transformation for Northbeam's website, enhancing its brand identity, user experience, and conversion rates through strategic research, design, and implementation.",
-  url: "/work/northbeam",
+  url: "https://argzonhaziraj.com/work/northbeam",
+  openGraph: {
+    title: "Northbeam - Argzon's Portfolio",
+    description:
+      "A complete UX/UI transformation for Northbeam's website, enhancing its brand identity, user experience, and conversion rates through strategic research, design, and implementation.",
+    url: "https://argzonhaziraj.com/work/northbeam",
+    siteName: "Argzon Haziraj",
+    images: [
+      {
+        url: "https://argzonhaziraj.com/assets/opengraph-images/northbeam-og-image.jpg",
+        width: 1200,
+        height: 675,
+        alt: "Argzon Haziraj - Interface Designer",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Northbeam - Argzon's Portfolio",
+    description:
+      "A complete UX/UI transformation for Northbeam's website, enhancing its brand identity, user experience, and conversion rates through strategic research, design, and implementation.",
+    creator: "@Argzon6",
+    images: [
+      "https://argzonhaziraj.com/assets/opengraph-images/northbeam-og-image.jpg",
+    ],
+  },
+  robots: {
+    index: true,
+    follow: true,
+  },
+  icons: {
+    icon: "/favicon.ico",
+  },
 };
 
 export default function NorthbeamLayout({ children }) {
--- src/app/work/talentally/layout.js
+++ src/app/work/talentally/layout.js
@@ -1,8 +1,43 @@
 export const metadata = {
+  metadataBase: new URL("https://argzonhaziraj.com"),
   title: "TalentAlly - Argzon's Portfolio",
   description:
     "A complete UI/UX redesign of TalentAlly's (formerly: IPDN) website, transforming an outdated platform into a modern, user-friendly experience. This case study showcases my design process, collaboration, and final impactful results.",
-  url: "/work/talentally",
+  url: "https://argzonhaziraj.com/work/talentally",
+  openGraph: {
+    title: "TalentAlly - Argzon's Portfolio",
+    description:
+      "A complete UI/UX redesign of TalentAlly's (formerly: IPDN) website, transforming an outdated platform into a modern, user-friendly experience. This case study showcases my design process, collaboration, and final impactful results.",
+    url: "https://argzonhaziraj.com/work/talentally",
+    siteName: "Argzon Haziraj",
+    images: [
+      {
+        url: "https://argzonhaziraj.com/assets/opengraph-images/talentally-og-image.jpg",
+        width: 1200,
+        height: 675,
+        alt: "Argzon Haziraj - Interface Designer",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "TalentAlly - Argzon's Portfolio",
+    description:
+      "A complete UI/UX redesign of TalentAlly's (formerly: IPDN) website, transforming an outdated platform into a modern, user-friendly experience. This case study showcases my design process, collaboration, and final impactful results.",
+    creator: "@Argzon6",
+    images: [
+      "https://argzonhaziraj.com/assets/opengraph-images/talentally-og-image.jpg",
+    ],
+  },
+  robots: {
+    index: true,
+    follow: true,
+  },
+  icons: {
+    icon: "/favicon.ico",
+  },
 };
 
 export default function TalentallyLayout({ children }) {
