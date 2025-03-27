3567ddade34ca75237b7c2a7b49db48d37600bdc
Argzon <argzon01@gmail.com>
2025-03-27 02:55:03 +0100

    Fix

--- src/app/layout.js
+++ src/app/layout.js
@@ -24,14 +24,6 @@ export default function RootLayout({ children }) {
             name="apple-mobile-web-app-title"
             content="Argzon's Portfolio"
           />
-          <meta property="og:image" content="<generated>" />
-          <meta property="og:image:type" content="<generated>" />
-          <meta property="og:image:width" content="<generated>" />
-          <meta property="og:image:height" content="<generated>" />
-          <meta name="twitter:image" content="<generated>" />
-          <meta name="twitter:image:type" content="<generated>" />
-          <meta name="twitter:image:width" content="<generated>" />
-          <meta name="twitter:image:height" content="<generated>" />
         </head>
         <body>
           <PostHogProvider>
